8d8a591050b108327c162939fdc5c53bf26f35c9
haninalnaser <alnaserhanin16@gmail.com>
2026-09-14 20:06:47 -0500

    Add portfolio visualizations

--- tech_sales_analysis.R
+++ tech_sales_analysis.R
@@ -263,5 +263,66 @@ print(
   )
 )
 
+# ------------------------------------------------------------
+# 9. Portfolio visualizations
+# ------------------------------------------------------------
+
+# Create a folder for portfolio charts
+if (!dir.exists("figures")) {
+  dir.create("figures")
+}
+
+# Chart 1: Salary vs. NPS
+png("figures/salary_vs_nps.png", width = 900, height = 600)
+plot(
+  TechSales$Salary,
+  TechSales$NPS,
+  xlab = "Salary",
+  ylab = "NPS",
+  main = "Salary vs. NPS",
+  pch = 16,
+  cex = 0.5
+)
+abline(
+  lm(NPS ~ Salary, data = TechSales),
+  lwd = 2
+)
+dev.off()
+
+# Chart 2: Certificates vs. NPS
+png("figures/certificates_vs_nps.png", width = 900, height = 600)
+plot(
+  TechSales$Certficates,
+  TechSales$NPS,
+  xlab = "Number of Certificates",
+  ylab = "NPS",
+  main = "Certificates vs. NPS",
+  pch = 16,
+  cex = 0.5
+)
+abline(
+  lm(NPS ~ Certficates, data = TechSales),
+  lwd = 2
+)
+dev.off()
+
+# Chart 3: Average NPS by Personality
+personality_nps <- aggregate(
+  NPS ~ Personality,
+  data = TechSales,
+  FUN = mean
+)
+
+png("figures/nps_by_personality.png", width = 900, height = 600)
+barplot(
+  personality_nps$NPS,
+  names.arg = personality_nps$Personality,
+  xlab = "Personality Type",
+  ylab = "Average NPS",
+  main = "Average NPS by Personality Type",
+  ylim = c(0, 10)
+)
+dev.off()
+
 # End of analysis
 
